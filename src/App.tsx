caf33befa5e9ce876a779f3f56adde826640d0e4
WireliteSoft <frostic1@yahoo.com>
2026-02-20 20:25:39 -0800

    resize

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
         <div className="max-w-7xl mx-auto px-6 py-4">
           <div className="flex justify-between items-center">
             <div className="flex items-center">
-              <img src={logo} alt="Candle Haven" className="h-14 w-auto" />
+              <img src={logo} alt="Candle Haven" className="h-20 w-auto object-contain" />
             </div>
 
             <div className="flex items-center gap-4">
